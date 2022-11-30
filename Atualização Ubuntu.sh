349eb8b9aa3fe842e10e8f46b0cf4051dece4f1a
Robson Nakane <97422340+robsonnakane@users.noreply.github.com>
2022-11-30 10:53:54 -0300

    Acréscimo Firefox

--- Atualização Ubuntu.sh
+++ Atualização Ubuntu.sh
@@ -4,21 +4,22 @@
 ###Atualização completa do sistema###
 sudo apt autoremove -y;
 sudo apt update -y;
+sudo apt upgrade -y;
 sudo apt full-upgrade -y;
 
 
 ##Execução do arquivo de atualização no terminal##
-#/var/home/robertonakane/Documentos/'Atualização Ubuntu.sh'#
+#/var/home/robsonnakane/Documentos/'Atualização Ubuntu.sh'#
 
 #Edição do arquivo no terminal#
-#nano /var/home/robertonakane/Documentos/'Atualização Ubuntu.sh'#
+#nano /var/home/robsonnakane/Documentos/'Atualização Ubuntu.sh'#
 
 
 ##Instalação de programas e Flatpak##
 sudo apt install neofetch -y; sudo apt install simple-scan -y; sudo apt install gnome-tweaks -y; sudo apt install java-latest-openjdk -y; sudo apt install btop -y; sudo apt install flatpak -y; sudo apt install gnome-software-plugin-flatpak -y; flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo;
 
 ##Instalação dos programas Flatpak##
-#flatpak install flathub com.spotify.Client -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub com.valvesoftware.Steam -y; flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.kde.kdenlive -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.skype.Client -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub org.gnome.Extensions -y; flatpak install flathub org.gnome.Boxes -y; flatpak install flathub com.transmissionbt.Transmission -y; flatpak install flathub org.mypaint.MyPaint -y; flatpak install flathub org.mozilla.Thunderbird -y; flatpak install flathub org.raspberrypi.rpi-imager -y; flatpak install flathub org.fedoraproject.MediaWriter -y; flatpak install flathub org.gnome.gedit -y; flatpak install flathub com.google.Chrome -y; flatpak install flathub org.gnome.Firmware -y; flatpak install flathub org.atheme.audacious -y; flatpak install flathub ca.littlesvr.asunder -y; flatpak install flathub ar.com.tuxguitar.TuxGuitar -y;
+#flatpak install flathub com.spotify.Client -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub com.valvesoftware.Steam -y; flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.kde.kdenlive -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.skype.Client -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub org.gnome.Extensions -y; flatpak install flathub org.gnome.Boxes -y; flatpak install flathub com.transmissionbt.Transmission -y; flatpak install flathub org.mypaint.MyPaint -y; flatpak install flathub org.mozilla.Thunderbird -y; flatpak install flathub org.raspberrypi.rpi-imager -y; flatpak install flathub org.fedoraproject.MediaWriter -y; flatpak install flathub org.gnome.gedit -y; flatpak install flathub com.google.Chrome -y; flatpak install flathub org.gnome.Firmware -y; flatpak install flathub org.atheme.audacious -y; flatpak install flathub ca.littlesvr.asunder -y; flatpak install flathub ar.com.tuxguitar.TuxGuitar -y; flatpak install flathub org.mozilla.firefox -y;
 
 ##Atualização do Flatpak##
 #flatpak update -y;
